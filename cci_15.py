1440f5db0da8ef4ec3e134ce7402f8dfa524a323
Pavel Komarov <pvlkmrv@gmail.com>
2021-04-17 14:20:26 -0600

    finished chapter 15

--- cci_15.py
+++ cci_15.py
@@ -31,10 +31,10 @@
 # they're sufficient to implement the spirit of the answer.
 # https://docs.python.org/3/library/threading.html
 # https://realpython.com/intro-to-python-threading/
-from threading import Thread, Lock
+from threading import Thread, Lock, Semaphore, Barrier
 from time import sleep
 
-def three(philosophers, race_condition=True):
+def three(philosophers, race_condition=True): # table simulation
 	N = len(philosophers)
 	
 	if race_condition: # chaotic, everyone trying to eat at once
@@ -120,7 +120,7 @@ class Four:
 
 		# Look for cycle, using graph coloring algorithm
 		white = {l for l in self.graph} # nodes get removed as we explore
-		for u in lock_ndxs: # for generalized, you'd do this for all nodes in the graph, but here only need the added ones
+		for u in lock_ndxs: # in general you'd do this for all nodes in a graph, but here we only need check the new ones
 			if u in white and self._find_cycle(white, set(), u): # no nodes are grey to start, so empty set
 				# Then cycle found! Remove new edges, and return False immediately
 				for v,w in new_edges:
@@ -137,25 +137,24 @@ class Four:
 		grey.add(u)
 
 		for v in self.graph.get(u, []): # get() so the defaultdict doesn't create empty entries for sink nodes
-			if v in grey:
+			if v in grey: # if we've found a node along our exploration path
 				return True
+			if v in white and self._find_cycle(white, grey, v): # if node not yet explored, then try to find cycle
+				return True # if also a cycle found, then squawk
 
-			if v in white and self._find_cycle(white, grey, v):
-				return True
-
-		grey.remove(u) # change color from grey to black (exploring -> explored)
-		return False	# Not keeping black set, because it's not important.
+		grey.remove(u) # No cycles found along this DFS. Change color from grey to black (exploring -> explored)
+		return False	# Not keeping black set, because it's not used.
 
 safety_third = Four()
 for i in range(10): safety_third.register(Lock())
 A = [1,2,3,4]
 B = [1,3,5]
 C = [7,5,9,2]
-assert safety_third.request(A)
+assert safety_third.request(A) # valid
 assert str(safety_third.graph) == "defaultdict(<class 'set'>, {1: {2}, 2: {3}, 3: {4}})"
-assert safety_third.request(B)
+assert safety_third.request(B) # valid
 assert str(safety_third.graph) == "defaultdict(<class 'set'>, {1: {2, 3}, 2: {3}, 3: {4, 5}})"
-assert not safety_third.request(C)
+assert not safety_third.request(C) # not valid, so graph comes back same as it was
 assert str(safety_third.graph) == "defaultdict(<class 'set'>, {1: {2, 3}, 2: {3}, 3: {4, 5}})"
 try:
 	safety_third.request([10,12])
@@ -163,5 +162,93 @@ try:
 except ValueError:
 	assert True
 
+class Five:
+	def __init__(self):
+		self.sem1 = Semaphore() # Has to be a semaphore so multiple Threads can touch it.
+		self.sem2 = Semaphore() # If the main thread acquires locks, then only the main thread
+		self.sem1.acquire() # can later release the locks, whereas anyone can acquire and
+		self.sem2.acquire() # release on semaphores.
 
+	def first(self):
+		if verbose: print("first"); sleep(0.2)
+		order.append("first")
+		self.sem1.release()
 
+	def second(self):
+		self.sem1.acquire()
+		self.sem1.release()
+		if verbose: print("second"); sleep(0.2)
+		order.append("second")
+		self.sem2.release()
+
+	def third(self):
+		self.sem2.acquire()
+		self.sem2.release()
+		if verbose: print("third"); sleep(0.2)
+		order.append("third")
+
+foo = Five()
+verbose = False
+order = []
+A = Thread(target=lambda x: x.first(), args=[foo])
+B = Thread(target=lambda x: x.second(), args=[foo])
+C = Thread(target=lambda x: x.third(), args=[foo])
+for thread in [C, B, A]:
+	thread.start()
+C.join() # let all threads finish before we check the value of `order`
+assert order == ['first', 'second', 'third']
+
+# Six
+# If method A is synchonized, then two threads can not execute it concurrently, because whoever does
+# first will hold the lock for that method, until method execution is finished. Caveat: If the two
+# threads hold different instances of the object, then of course they can execute those objects' own
+# method As in parallel, because synchronization is instance-specific. If B is not synchronized, then
+# sure, the two methods can execute in parallel. If B were synchronized, it uses the same lock as A
+# within the instance, so they couldn't execute in parallel.
+
+def seven(n):
+	# I tried for a while to do this with a variable accessible to all, which one thread would
+	# increment and others would check against. I needed the counting thread to go first, which I
+	# implemented with a few semaphores, but there was too much jostle in the system when removing
+	# the sleep statements. I don't know why or how, but I'd see one of my threads check twice while
+	# another wouldn't at all, even though all should have been hitting their wait() statements.
+	# Doens't make any sense. A way around this is to give each thread its own i to increment, which
+	# is what I did when I solved this on Leetcode, and is what Gayle does in the solutions.
+	out = []
+	barrier = Barrier(4) # The Barrier is a convenient way to solve this. The solutions use a static
+							# lock with a bunch of thread instances all trying to chaotically print
+	def count():			# at the same time, most just recognizing they can't and releasing.
+		for i in range(1, n+1):
+			if i % 3 != 0 and i % 5 != 0:
+				out.append(str(i))
+			barrier.wait()
+
+	def divthree():
+		for i in range(1, n+1):
+			if i % 3 == 0 and i % 5 != 0:
+				out.append(str(i) + " Fizz")
+			barrier.wait()
+
+	def divfive():
+		for i in range(1, n+1):
+			if i % 3 != 0 and i % 5 == 0:
+				out.append(str(i) + " Buzz")
+			barrier.wait()
+
+	def divthreefive():
+		for i in range(1, n+1):
+			if i % 3 == 0 and i % 5 == 0:
+				out.append(str(i) + " FizzBuzz")
+			barrier.wait()
+
+	threads = [Thread(target=count),
+				Thread(target=divthree),
+				Thread(target=divfive),
+				Thread(target=divthreefive)]
+	for t in threads: t.start()
+	for t in threads: t.join()
+	return out
+
+assert seven(30) == ['1', '2', '3 Fizz', '4', '5 Buzz', '6 Fizz', '7', '8', '9 Fizz', '10 Buzz',
+					'11', '12 Fizz', '13', '14', '15 FizzBuzz', '16', '17', '18 Fizz', '19', '20 Buzz',
+					'21 Fizz', '22', '23', '24 Fizz', '25 Buzz', '26', '27 Fizz', '28', '29', '30 FizzBuzz']
